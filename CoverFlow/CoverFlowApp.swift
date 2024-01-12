b3775da92643675347999c799f903bc1d387b79f
Rob Sturcke <rob@rslabs.dev>
2024-01-12 17:49:20 -0500

    Initial Commit

---
+++ CoverFlow/CoverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverFlowApp.swift
+//  CoverFlow
+//
+//  Created by Rob Sturcke on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CoverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
