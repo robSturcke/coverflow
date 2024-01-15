a013ecb60edc6bcc7a7e40ad3d0a0398b5ccc591
Rob Sturcke <rob@rslabs.dev>
2024-01-15 12:31:04 -0500

    coverflow rects

--- CoverFlow/ContentView.swift
+++ CoverFlow/ContentView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var items: [CoverFlowItem] = [.red, .blue, .green, .yellow, .primary].compactMap{
+    return .init(color: $0)
+  }
   var body: some View {
     NavigationStack {
       VStack {
-        
+        CoverFlowView(itemWidth: 280, items: items)  { item in
+            RoundedRectangle(cornerRadius: 20)
+            .fill(item.color.gradient)
+        }
+        .frame(height: 180)
       }
       .navigationTitle("CoverFlow")
     }
